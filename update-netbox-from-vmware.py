93db819aa4b2bd0f8913041721b7c6f54c485d02
Michael N. Jensen <michael.jensen@dustin.dk>
2020-03-11 10:28:31 +0100

    Import initial script, that can list clusters in netbox/vcenter

---
+++ update-netbox-from-vmware.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+import pynetbox
+import atexit
+import ipaddress
+import urllib3
+import logging 
+import os
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+vcenter_session = None
+vcenter_content = None
+netbox_client = None
+logger = None
+
+class VMwareCluster:
+    def __init__(self, name, vcenter_persistent_id):
+        self.name = name
+        self.vcenter_persistent_id = vcenter_persistent_id
+
+class NetboxCluster:
+    def __init__(self, name, vcenter_persistent_id):
+        self.name = name
+        self.vcenter_persistent_id = vcenter_persistent_id
+
+def get_vcenter_clusters():
+    vcenter_clusters = []
+    
+    # Get a list of datacenters in the vcenter
+    vcenter_datacenters = vcenter_content.rootFolder.childEntity
+
+    # We only have one datacenter, but lets loop through the list of them anyway
+    for vcenter_datacenter in vcenter_datacenters:
+        for vcenter_cluster in vcenter_datacenter.hostFolder.childEntity:
+            vcenter_clusters.append( VMwareCluster( name = vcenter_cluster.name, 
+                                                    vcenter_persistent_id = vcenter_cluster._moId) )
+
+    return vcenter_clusters
+
+def get_netbox_clusters():
+    netbox_clusters = []
+
+    nb_clusters = netbox_client.virtualization.clusters.all()
+    
+    for nb_cluster in nb_clusters:
+        if str(nb_cluster.type) == "vSphere":
+            netbox_clusters.append( NetboxCluster( name = nb_cluster.name, 
+                                                   vcenter_persistent_id = nb_cluster.custom_fields.get('vcenter_persistent_id')) )
+        
+    return netbox_clusters
+
+def update_netbox_clusters():
+
+    vcenter_clusters = get_vcenter_clusters()
+    netbox_clusters = get_netbox_clusters()
+
+    # Find clusters present in netbox, but not in vsphere, and add comment
+    # about it, on the netbox cluster object
+    for nbc1 in netbox_clusters:
+        if any(vc1.vcenter_persistent_id == nbc1.vcenter_persistent_id for vc1 in vcenter_clusters ):
+            logging.info(f"Cluster: {nbc1.name} with vCenter_ID: {nbc1.vcenter_persistent_id} exists in vcenter, nothing to do")
+        else:
+            logging.info(f"Cluster: {nbc1.name} with vCenter_ID: {nbc1.vcenter_persistent_id} does NOT exists in vcenter, adding comment to netbox")
+            
+    
+def debug_print_object_info(obj):
+    print(">x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>x>")
+    for attr in dir(obj):
+        print("obj.%s = %r" % (attr, getattr(obj, attr)))
+    print("<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<z<")
+
+def initialize_vcenter_connection():
+    global vcenter_session
+    global vcenter_content
+
+    vcenter_hostname = os.environ.get("VCENTER_HOSTNAME")
+    vcenter_username = os.environ.get("VCENTER_USERNAME")
+    vcenter_password = os.environ.get("VCENTER_PASSWORD")
+
+    if not vcenter_hostname or not vcenter_username or not vcenter_password:
+        logger.error("vCenter hostname/username/password is not set via environment variables")
+        raise SystemExit(-1)
+
+    vcenter_session = connect.SmartConnectNoSSL( host=vcenter_hostname,
+                                                 user=vcenter_username,
+                                                 pwd=vcenter_password,
+                                                 port=int(443) )
+
+    atexit.register(connect.Disconnect, vcenter_session)
+
+    vcenter_content = vcenter_session.RetrieveContent()
+
+def initialize_netbox_client():
+    global netbox_client
+
+    netbox_url = os.environ.get("NETBOX_API_URI")
+    netbox_token = os.environ.get("NETBOX_API_TOKEN")
+
+    if not netbox_url or not netbox_token:
+        logger.error("Netbox url/token is not set via environment variables")
+        raise SystemExit(-1)
+
+    netbox_client = pynetbox.api (
+        url = netbox_url,
+        token = netbox_token,
+        ssl_verify = False
+    )
+
+def initialize_logging():
+    global logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    
+    logger.addHandler(ch)
+    
+def main():
+    initialize_logging()
+
+    # Disable warnings about SSL
+    urllib3.disable_warnings()
+    
+    initialize_vcenter_connection()
+    initialize_netbox_client()
+
+    update_netbox_clusters()
+
+if __name__ == "__main__":
+    main()
