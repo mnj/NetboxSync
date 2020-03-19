9f0b77fbb09d23e7678c2042fd81fc2aaef74c5c
Michael N. Jensen <michael.jensen@dustin.dk>
2020-03-19 11:40:52 +0100

    Add code to update VMs in netbox

--- update-netbox-from-vmware.py
+++ update-netbox-from-vmware.py
@@ -28,7 +28,7 @@ class NetboxCluster:
         self.name = name
         self.vcenter_persistent_id = vcenter_persistent_id
         self.raw_netbox_api_record = raw_netbox_api_record
-        
+
 class VMwareVM:
     def __init__(self, name, uuid, vcpu, memory_mb, comment, power_state, vmtools_status, primary_ipaddress, is_template, custom_attributes, cluster_name):
         self.name = name
@@ -115,7 +115,7 @@ def update_netbox():
                 cluster_type = netbox_client.virtualization.cluster_types.get(name="vSphere")
                 custom_fields = {}
                 custom_fields["vcenter_persistent_id"] = vc2.vcenter_persistent_id
-
+                
                 nbc2_create = netbox_client.virtualization.clusters.create( name = vc2.name,
                                                                             type = cluster_type.id,
                                                                             custom_fields = custom_fields )
@@ -123,7 +123,89 @@ def update_netbox():
             except Exception as ex:
                 logger.warn("Failed creating the cluster object in netbox")
                 logger.exception(ex)
+
+    vcenter_vms = get_vcenter_vms()
+    netbox_vms = get_netbox_vms()
+
+    # Find VMs present in netbox, but not in vsphere, and add comment
+    # about it, on the netbox cluster object.
+    # And update existing vms with latest information from vcenter if
+    # they already exists, and something has changed
+    for nbvm1 in netbox_vms:
+        if any(vcvm1.uuid == nbvm1.vcenter_persistent_id for vcvm1 in vcenter_vms):
+            logger.info(f"VM: {nbvm1.name} with vCenter_ID: {nbvm1.vcenter_persistent_id} exists in vcenter, checking if anything has changed")
+            foundChanges = False
+
+            # next will raise errors if not found!, but we check above with any() so it is safe here
+            vcvm = next(x for x in vcenter_vms if x.uuid == nbvm1.vcenter_persistent_id)
+
+            try:
+                nbvm1_update = netbox_client.virtualization.virtual_machines.get(nbvm1.raw_netbox_api_record.id)
+
+                if int(vcvm.vcpu) != int(nbvm1.raw_netbox_api_record.vcpus):
+                    foundChanges = True
+                    nbvm1_update.vcpus = vcvm.vcpu
+                    logger.info(f"Found change, VC VM vcpu: {vcvm.vcpu}, NB VM vcpu: {nbvm1.raw_netbox_api_record.vcpus}")
+                elif int(vcvm.memory_mb) != int(nbvm1.raw_netbox_api_record.memory):
+                    foundChanges = True
+                    nbvm1_update.memory = vcvm.memory_mb
+                    logger.info(f"Found change, VC VM memory: {vcvm.memory_mb}, NB VM memory: {nbvm1.raw_netbox_api_record.memory}")
+                elif vcvm.comment != nbvm1.raw_netbox_api_record.comments and vcvm.comment is not None:
+                    foundChanges = True
+                    nbvm1_update.comments = vcvm.comment
+                    logger.info(f"Found change, VC VM comment: {vcvm.comment}, NB VM comment: {nbvm1.raw_netbox_api_record.comments}")
+
+                if foundChanges:
+                    logger.info(f"Update VM: {nbvm1.name} in netbox, since changes was detected!")
+                    
+                    if nbvm1_update.save():
+                        logger.info("Successfully updated VM object in netbox")
+            except Exception as ex:
+                logger.warn("Failed updating the VM object in netbox")
+                logger.exception(ex)
+        else:
+            logger.info(f"VM: {nbvm1.name} with vCenter_ID: {nbvm1.vcenter_persistent_id} does NOT exists in vcenter, adding comment to netbox")
+            
+            try:
+                nbvm1_update = netbox_client.virtualization.virtual_machines.get(nbvm1.raw_netbox_api_record.id)
+                nbvm1_update.comments = "No longer present in vCenter, verify manually, and delete this object in netbox"
+                if nbvm1_update.save():
+                    logger.info("Successfully updated VM object in netbox")
+            except Exception as ex:
+                logger.warn("Failed updating the VM object in netbox")
+                logger.exception(ex)
+
+    # Find vms present in vcenter, but not in netbox
+    for vcvm2 in vcenter_vms:
+        if any(nbvm2.vcenter_persistent_id == vcvm2.uuid for nbvm2 in netbox_vms):
+            logger.info(f"VM: {vcvm2.name} with vCenter_ID: {vcvm2.uuid} exists in netbox, nothing to do")
+        else:
+            logger.info(f"VM: {vcvm2.name} with vCenter_ID: {vcvm2.uuid} does NOT exists in netbox, adding the cluster to netbox")
+
+            try:
+                netbox_cluster_id = _netbox_get_cluster_id(netbox_clusters, vcvm2.cluster_name)
+                custom_fields = {}
+                custom_fields["vcenter_persistent_id"] = vcvm2.uuid
                 
+                comment = ""
+                if vcvm2.comment is not None:
+                    comment = vcvm2.comment
+
+                # Our company specific custom netbox attribute, if it's defined
+                if "SystemID" in vcvm2.custom_attributes:
+                    custom_fields["SystemID"] = vcvm2.custom_attributes["SystemID"]
+                
+                nbvm2_create = netbox_client.virtualization.virtual_machines.create( name = vcvm2.name,
+                                                                                     cluster = netbox_cluster_id,
+                                                                                     comments = comment,
+                                                                                     custom_fields = custom_fields,
+                                                                                     vcpus = vcvm2.vcpu,
+                                                                                     memory = vcvm2.memory_mb )
+                logger.info(f"VM object: { nbvm2_create } created successfully in netbox")
+            except Exception as ex:
+                logger.warn("Failed creating the VM object in netbox")
+                logger.exception(ex)
+
 def get_vcenter_vms():
     vcenter_vms = []
     
